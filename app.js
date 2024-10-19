24696b2da9171c61161eba79483a83af9b3ccb5b
sagarpoudel638 <sagarpoudel638@gmail.com>
2024-10-19 15:11:23 +1100

    added get and post controller

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 const app = express();
 
 const PORT = 8000;
 const __dirname = path.resolve();
-console.log(__dirname)
+console.log(__dirname);
 // app.get("/",(req,res)=>{
 //     // console.log("we got the request")
 //     // res.send("")
 // })
 
-// serve static file from the public directory 
+// serve static file from the public directory
+
+app.use(express.static(path.join(__dirname, "public")));
 
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended:true}));
+//Homepage controller
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/src/index.html`);
+});
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`http://localhost:${PORT}`);
+});
 
-app.get("/", (req, res)=>{
-    res.sendFile(`${__dirname}/src/index.html`)
-})
-app.listen(PORT,(error)=>{
-    error?console.log(error)
-    :console.log(`http://localhost:${PORT}`)
-})
+// user registration controller
+app.get("/registration", (req, res) => {
+    
+  console.log("request received registration");
+  res.sendFile(`${__dirname}/src/registration.html`);
+});
 
-app.get("/get-user",(req,res)=>{
+app.post("/registration", (req, res) => {
+    console.log(req.body)
+  console.log("request received registration");
+  res.sendFile(`${__dirname}/src/registration.html`);
+});
 
-    res.json({
-        firstName:"Sagar",
-        lastName:"Poudel"
-    })
-})
+// user login controller
+app.get("/login", (req, res) => {
+  console.log("request received login");
+  res.sendFile(`${__dirname}/src/login.html`);
+});
+
+// app.get("/api/v1/get-user",(req,res)=>{
+
+//     res.json({
+//         firstName:"Sagar",
+//         lastName:"Poudel"
+//     })
+// })
